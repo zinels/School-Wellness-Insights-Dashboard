48fdfd9a52d6243600ee809ad325ae09e13851fe
Tanzeel Rahman <tanzeelriyan2015@gmail.com>
2025-05-25 08:34:07 +0600

    index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChartComponent from './Chart';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ChartComponent />
+  </React.StrictMode>
+);
